9d4f8e6b6d84a8836cc2bfc33ce6a6965171992b
M Shan <mshan18719@gmail.com>
2026-04-12 11:12:26 +0500

    position alignment of player

--- js/entities/player.js
+++ js/entities/player.js
@@ -48,7 +48,7 @@ export class Player {
         this.jumpWasDown = false;
 
         const groundRow = map.length - 1;
-        this.x = 180;
+        this.x = 300;
         this.y = groundRow * tileSize - this.height;
 
         this.facing = 1;
